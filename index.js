164be829080275f630448ce50c77f76fd9d22cd3
Michael Doherty <dohertym12@hotmail.com>
2015-12-01 23:08:42 +0000

    reading web page contents from index.html

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var indexhtmlbuf = fs.readFileSync('/home/ubuntu/hw3/bitstarter/index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(indexhtmlbuf.toString())
 })
 
 app.listen(app.get('port'), function() {
