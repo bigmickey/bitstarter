481f8aa34c5b19967c952f180442b69fe27d5640
Michael Doherty <dohertym12@hotmail.com>
2015-12-02 22:33:00 +0000

    trying local path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fs = require('fs')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var indexhtmlbuf = fs.readFileSync('/home/ubuntu/hw3/bitstarter/index.html')
+var indexhtmlbuf = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
   response.send(indexhtmlbuf.toString())
